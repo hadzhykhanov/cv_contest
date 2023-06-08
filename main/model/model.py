533a0a0f51d2e4a95c9e9fda54efb9dfe7576029
VladGadzikhanov <gadzikenzo@gmail.com>
2023-06-08 20:16:32 +0300

    updated

--- main/model/model.py
+++ main/model/model.py
@@ -5,7 +5,7 @@ from torchvision import models
 
 
 class FeatureExtractor(Module):
-    def __init__(self, input_size=(64, 320), output_len=120):
+    def __init__(self, input_size=(64, 320), output_len=100):
         super(self.__class__, self).__init__()
 
         h, w = input_size
@@ -119,9 +119,9 @@ class CRNN(Module):
         self,
         num_chars,
         cnn_input_size=(64, 320),
-        cnn_output_len=120,
+        cnn_output_len=100,
         rnn_hidden_size=512,
-        rnn_num_layers=3,
+        rnn_num_layers=2,
         rnn_dropout=0.05,
         rnn_bidirectional=True,
     ):
