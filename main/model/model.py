9163a4dd79a01db9b323ba3f539ebc95cbb95c0f
VladGadzikhanov <gadzikenzo@gmail.com>
2023-06-08 12:21:36 +0300

    updated

--- main/model/model.py
+++ main/model/model.py
@@ -120,10 +120,10 @@ class CRNN(Module):
         num_chars,
         cnn_input_size=(64, 320),
         cnn_output_len=100,
-        rnn_hidden_size=512,
-        rnn_num_layers=2,
+        rnn_hidden_size=128,
+        rnn_num_layers=1,
         rnn_dropout=0.05,
-        rnn_bidirectional=True,
+        rnn_bidirectional=False,
     ):
         super(self.__class__, self).__init__()
 
