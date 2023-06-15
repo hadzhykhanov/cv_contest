18069f666266c72a38d42ca25518474183190deb
VladGadzikhanov <gadzikenzo@gmail.com>
2023-06-15 03:28:49 +0300

    Update model.py

--- main/rotation_part/model/model.py
+++ main/rotation_part/model/model.py
@@ -8,7 +8,7 @@ class ResNet18(Module):
         super(self.__class__, self).__init__()
         self.model = models.efficientnet_b4(weights=True)
 
-        self.model.classifier[1] = Linear(in_features=2560, out_features=num_classes)
+        self.model.classifier[1] = Linear(in_features=1792, out_features=num_classes)
         # self.resnet = models.resnet18(pretrained=True)
         # self.resnet.fc = torch.nn.Linear(self.resnet.fc.in_features, num_classes)
 
