87954bb128e85c0fb7f518076d0062a4e058653d
VladGadzikhanov <gadzikenzo@gmail.com>
2023-06-15 03:51:24 +0300

    Update training_pipeline.py

--- main/rotation_part/training_pipeline.py
+++ main/rotation_part/training_pipeline.py
@@ -92,6 +92,10 @@ def run_training(config: DictConfig):
 
         test_accuracy = accuracy_score(test_targets, test_preds)
 
+        print(
+            list(filter(lambda x: x[0] != x[1], zip(files, test_targets, test_preds)))
+        )
+
         for value in angle_to_idx.values():
             iterator = zip(test_targets, test_preds)
             lst = list(filter(lambda x: x[0] == value, iterator))
