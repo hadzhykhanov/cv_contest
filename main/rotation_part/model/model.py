eccc5876db34c0bced7e79420968144659babf4b
VladGadzikhanov <gadzikenzo@gmail.com>
2023-06-15 03:05:10 +0300

    updated

--- main/rotation_part/model/model.py
+++ main/rotation_part/model/model.py
@@ -7,11 +7,13 @@ from torchvision import models
 class ResNet18(Module):
     def __init__(self, num_classes=4):
         super(self.__class__, self).__init__()
-        self.resnet = models.resnet18(pretrained=True)
-        self.resnet.fc = torch.nn.Linear(self.resnet.fc.in_features, num_classes)
+        self.model = models.efficientnet_b4(pretrained=True)
+        # self.resnet = models.resnet18(pretrained=True)
+        # self.resnet.fc = torch.nn.Linear(self.resnet.fc.in_features, num_classes)
+        self.model._fc = torch.nn.Linear(self.model._fc.in_features, num_classes)
 
     def forward(self, images, targets=None):
-        x = self.resnet(images)
+        x = self.model(images)
 
         if targets is not None:
             loss_func = CrossEntropyLoss()
